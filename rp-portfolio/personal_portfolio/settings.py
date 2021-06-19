3379a863571bf6e59df9f0b5db98a28f70312c05
kjonina <kjonina@gmail.com>
2021-06-19 23:40:10 +0100

    fixing

--- rp-portfolio/personal_portfolio/settings.py
+++ rp-portfolio/personal_portfolio/settings.py
@@ -36,8 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'hello_world',
+    'django.contrib.staticfiles'
+    'projects',
 ]
 
 MIDDLEWARE = [
